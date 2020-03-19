4f5b3ab98ed2826fa8244011d6b9a1579b727dd1
Emmanuel Morales <emmanuel.morales@headspring.com>
2020-03-19 15:24:44 -0500

    Create displayfor html helper equivalent in a new tag helper

---
+++ ContosoUniversity/Infrastructure/TagHelpers/FormBlockDisplayTagHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using ContosoUniversity.Data;
+using ContosoUniversity.Infrastructure.Attributes;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using StackExchange.Profiling.Internal;
+
+namespace ContosoUniversity.Infrastructure.TagHelpers
+{
+    [HtmlTargetElement("form-block-display", Attributes = ForAttributeName, TagStructure = TagStructure.WithoutEndTag)]
+    public class FormBlockDisplayTagHelper : TagHelper
+    {
+        private const string ForAttributeName = "asp-for";
+
+        private readonly IHtmlGenerator _generator;
+        private readonly SchoolContext _dbContext;
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        [HtmlAttributeName(ForAttributeName)]
+        public ModelExpression For { get; set; }
+
+        public FormBlockDisplayTagHelper(IHtmlGenerator generator, SchoolContext dbContext)
+        {
+            _generator = generator;
+            _dbContext = dbContext;
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var label = GenerateLabel();
+            var displayField = GenerateDisplayField();
+
+            var classes = output.Attributes.FirstOrDefault(a => a.Name == "class")?.Value;
+
+            // set output
+            output.Attributes.SetAttribute("class", $"form-group {classes}");
+            output.TagName = "div"; // omit wrapper tag
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            // set content
+            output.PreContent.SetHtmlContent(label);
+            output.Content.SetHtmlContent(displayField);
+        }
+
+        private TagBuilder GenerateLabel() =>
+            _generator.GenerateLabel(ViewContext, For.ModelExplorer, For.Name, For.Metadata.DisplayName ?? For.Metadata.Name.BreakUpCamelCase(), new { @class = "control-label"} );
+
+        private TagBuilder GenerateDisplayField()
+        {
+            var displayField = new TagBuilder("div");
+            var text = For.ModelExplorer.GetSimpleDisplayText(); // TODO: set patterns to format by type
+            displayField.InnerHtml.SetContent(text);
+            return displayField;
+        }
+    }
+}
