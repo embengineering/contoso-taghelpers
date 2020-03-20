538c9662d51ce7d12de4a768ad93ee7f97981af1
Emmanuel Morales <emmanuel.morales@headspring.com>
2020-03-20 10:19:15 -0500

    Add new ValueFor attribute taghelper to assist with displaying model value
    This will help reduce markup and display the value of a model property directly in the dom where the value-for attribute was assign. This help centralize code in case we need to format by type in a centralized place consistently.

---
+++ ContosoUniversity/Infrastructure/TagHelpers/ValueForAttributeTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ContosoUniversity.Infrastructure.TagHelpers
+{
+    [HtmlTargetElement("*", Attributes = ValueForAttributeName)]
+    public class ValueForAttributeTagHelper : TagHelper
+    {
+        private const string ValueForAttributeName = "value-for";
+
+        [HtmlAttributeName(ValueForAttributeName)]
+        public ModelExpression ValueFor { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (ValueFor == null) return;
+
+            var displayValue = ValueFor.Model != null ? ValueFor.Model.ToString() : string.Empty;
+
+            output.Content.SetHtmlContent(displayValue);
+        }
+    }
+}
